0f6b70f5b71aa713362f11641e7a924860cbbfbc
ginpei <ginpei@gmail.com>
2017-06-28 20:26:06 -0700

    dummy text to make looking better

--- src/components/views/tasks/Show.vue
+++ src/components/views/tasks/Show.vue
@@ -4,7 +4,10 @@
 <template>
 	<base-layout backTo="/tasks/">
 		<template v-if="task">
-			<h1 routing-animation>{{task.title}}</h1>
+			<h1 routing-animation>Task: {{task.title}}</h1>
+			<p routing-animation>Ipsum odio similique officia magni libero Optio illum error magnam non commodi Perspiciatis tempore doloremque odit quo corrupti rerum Cumque placeat aperiam aperiam suscipit fuga at Tenetur cum repellat eveniet?</p>
+			<p routing-animation>Elit esse ducimus iusto odio iure neque! Impedit amet perspiciatis a fugit praesentium Facilis explicabo repellendus nisi ipsum odio perferendis dicta ea deserunt Illo molestiae excepturi repellat placeat ex. Eos.</p>
+			<p routing-animation>Amet inventore tempora voluptatibus optio quibusdam. Soluta hic repellat sequi consequatur magnam. Dolorum nemo illo ullam officia nesciunt? Magnam neque beatae autem aspernatur itaque Porro voluptates dolor non tempore aut?</p>
 		</template>
 		<template v-else>
 			<div>Wow</div>
